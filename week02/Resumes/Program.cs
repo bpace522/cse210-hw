51e03a6e0d7698f6aec2130d2e65e9ee535df187
lionenergyautomation <automation@lionenergy.com>
2025-05-13 09:22:31 -0600

    learning activity week2

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Ninja";
+        job1._startYear = 1834;
+        job1._endYear = 1954;
+
+        Job job2 = new Job();
+        job2._company = "Nintendo";
+        job2._jobTitle = "Playtester";
+        job2._startYear = 1955;
+        job2._endYear = 2025;
+
+        Resumes resume1 = new Resumes();
+        resume1._name = "Bradley Pace";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
+
     }
 }
